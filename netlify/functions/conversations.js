ea96d35cc512710b2db9d561a8f2b3176f4f4751
Christianmidm-BelWise <christian.mi.dm@gmail.com>
2026-06-11 00:29:09 +0200

    Update conversations.js

--- netlify/functions/conversations.js
+++ netlify/functions/conversations.js
@@ -1,5 +1,5 @@
-const { proxy } = require("./_reactify-backend");
+const { proxy } = require('./_reactify-backend');
 
-exports.handler = async function(event) {
-  return proxy(event, "/conversations", { method: event.httpMethod });
+exports.handler = async function (event) {
+  return proxy(event, '/conversations', { method: event.httpMethod });
 };
